81baad3c1f6ef11ac56e3ac8916d8145490755fb
Evan Hickle <ehickle@alumni.nd.edu>
2017-07-13 11:50:55 -0500

    Add stylesheet

--- app/helpers/session_helpers.rb
+++ app/helpers/session_helpers.rb
@@ -1,13 +1,13 @@
-module SessionHelper
+# module SessionHelper
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+#   def current_user
+#     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+#   end
 
-  def logged_in?
-    !!current_user
-  end
+#   def logged_in?
+#     !!current_user
+#   end
 
-end
+# end
 
-helpers SessionHelper
+# helpers SessionHelper
