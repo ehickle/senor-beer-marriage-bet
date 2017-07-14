c6891439e47cd7843da879156507cad9d53211e7
Evan Hickle <ehickle@alumni.nd.edu>
2017-07-14 11:17:00 -0500

    Add facebook login function

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -22,6 +22,16 @@ function statusChangeCallback(response) {
     });
   }
 
+
+
+(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1909757332598209";
+        fjs.parentNode.insertBefore(js, fjs);
+} (document, 'script', 'facebook-jssdk'));
+
 window.fbAsyncInit = function() {
     FB.init({
       appId      : '1909757332598209',
@@ -31,14 +41,6 @@ window.fbAsyncInit = function() {
     });
     FB.AppEvents.logPageView();
 };
-
-(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1909757332598209";
-        fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
