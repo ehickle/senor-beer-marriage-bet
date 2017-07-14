5dbeb95f43fa6dcd0db0bcaa9f8193a3aad6b1ae
Evan Hickle <ehickle@alumni.nd.edu>
2017-07-14 12:03:41 -0500

    Add errors to home page for facebook login

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -15,8 +15,8 @@ function statusChangeCallback(response) {
      })
   } else {
     // The person is not logged into your app or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
+    errors = ['Please try again']
+    $.get('/')
   }
 }
 
